02e6b18ae60b639103aa3c7ef14d765c5558c861
Damian <damiangrosu3@gmail.com>
2022-10-24 15:32:59 +0300

    added singleton

---
+++ src/Lab2/Singleton/Singleton.java
@@ -0,0 +1,19 @@
+package Lab2.Singleton;
+
+public class Singleton {
+
+    private static Singleton instance;
+
+    private Singleton() {}
+
+    public static Singleton getInstance(){
+        if (instance == null){
+            instance = new Singleton();
+        }
+        return instance;
+    }
+
+    public void doSmth(){
+        
+    }
+}
